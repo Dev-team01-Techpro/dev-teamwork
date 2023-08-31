8395e5f847f3a1d621893307bbfe287658e08074
zeynep secilmis <zeynepsecilmis629@gmail.com>
2023-08-31 17:53:17 +0100

    m

--- src/main/java/TestProject.java
+++ src/main/java/TestProject.java
@@ -1,7 +1,7 @@
 public class TestProject {
     public static void main(String[] args) {
         System.out.println("It is a test project");
-        System.out.println();
+        System.out.println("zeynep");
 
 
 
