c58f3955107bc55357d2b94026d08aaf03f56c99
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-09-08 22:44:48 +0200

    Loops teamwork

---
+++ src/main/java/loops/ForLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        /*
+            For
+            for (durum 1; durum 2; durum 3) {
+              // çalışacak kod bloğu
+            }
+        */
+
+    }
+}
---
+++ src/main/java/loops/WhileLoop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        /*
+        While
+        Parantezler içindeki değer doğru(true) olduğu sürece döngü dönmeye devam eder ve kod bloğu çalışır.
+        */
+
+    }
+}
