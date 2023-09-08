0cad71331c34414196ba03dc80eb8e69a0305290
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-09-08 23:39:19 +0200

    Loops For Loop

--- src/main/java/loops/ForLoop.java
+++ src/main/java/loops/ForLoop.java
@@ -1,5 +1,7 @@
 package loops;
 
+import java.util.Scanner;
+
 public class ForLoop {
     public static void main(String[] args) {
         /*
@@ -8,6 +10,35 @@ public class ForLoop {
               // çalışacak kod bloğu
             }
         */
+//        for(int i = 0; i <= 10; i += 2){
+//            System.out.println(i);
+//        }
+        /*
+            Java ile faktöriyel hesaplayan program yazıyoruz.
+            (n!) = 1 * 2 * 3 * 4 * ... * n
+            Ornek (5!) = 1 * 2 * 3 * 4 * 5 = 120
+         */
+
+        int n, total = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayi giriniz : ");
+        n = input.nextInt();
+
+
+        if(n>0){
+            for(int i = 1; i <= n; i++ ){
+                    total = total * i;
+                //    1   =   1   * 1
+                //    1   =   1   * 2
+                //    2   =   2   * 3
+                System.out.println("total = " + total);
+            }
+        } else {
+            System.out.println("Dogru sayi giriniz : ");
+        }
+
+
+
 
     }
 }
